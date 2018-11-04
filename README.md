01d7395a4edbbe95120c514ec5bcbaa7a0272028
alant <alan.htang@gmail.com>
2018-11-03 21:51:27 -0700

    add simpleStorage contract, client npm start works

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Aiming to make an intuitive starter project that utilizes popular frameworks for easy dapp development. 
+
+## build steps
+1. migrate the contract
+```bash
+truffle migrate
+```
+2. link contract build directory to react client
+I've only verified the following on MacOS. 
+```bash
+# For MacOS and Linux
+cd client/src
+ln -s ../../build/contracts contracts
+
+# For Windows 7, 8 and 10
+# Using a Command Prompt as Admin
+
+cd client/src
+mklink \D contracts ..\..\build\contracts
+```
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,49 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { DrizzleProvider } from 'drizzle-react';
+import SimpleStorage from './contracts/SimpleStorage.json';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { drizzleReducers, generateContractsInitialState } from 'drizzle';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+
+const drizzleOptions = {
+  contracts: [SimpleStorage]
+};
+
+const reducer = combineReducers({
+  ...drizzleReducers
+});
+
+const initialState = {
+  contracts: generateContractsInitialState(drizzleOptions)
+};
+
+// Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(
+    applyMiddleware(
+      sagaMiddleware
+    )
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render((
+  <DrizzleProvider options={drizzleOptions} store={store}>
+    <App />
+  </DrizzleProvider>
+  ),
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
