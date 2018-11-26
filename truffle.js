44b2a546934e551419e1d20c6b6eabe44300ae42
alant <7888785@qq.com>
2018-11-26 14:40:02 -0800

    add poa suport

--- truffle.js
+++ truffle.js
@@ -1,16 +1,24 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
  *     gasPrice: 10000000000,
  *   },
  */
+
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+// NOTE: Only required if not using localhost
+const mnemonic = process.env.ETH_MNEMONIC || '';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -21,6 +29,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    privatepoa: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'http://172.18.14.10:8502');
+      },
+      network_id: '888'
     }
   }
 };
